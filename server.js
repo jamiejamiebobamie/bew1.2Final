c1bd54b57b48acf8e7f1da46df90e555b8b746c1
jamiejamiebobamie <jmccrory@vt.edu>
2019-02-13 15:08:24 -0800

    reviewing past tutorial sections up to but not including the test section / section 4

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
+
 const mongoose = require('mongoose');
+
+// Set db
+const db = require('./data/reddit-db');
 
 //middleware for JSON data
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+
+//middleware for putting something when you post it
+const methodOverride = require('method-override');
+
+const Post = require('./models/post');
+const posts = require('./controllers/posts')(app);
+
+const port = process.env.PORT || 13000;
 
 // Use Body Parser
 app.use(bodyParser.json());
@@ -14,51 +26,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Add after body parser initialization!
 app.use(expressValidator());
 
-//middleware for putting something when you post it
-const methodOverride = require('method-override');
-
-const port = process.env.PORT || 17000;
-
 //must come below const app, but before routes
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // override with POST having ?_method=DELETE or ?_method=PUT
 app.use(methodOverride('_method'))
 
+// //heroku database.
+// mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes'), { useNewUrlParser: true });
 
-//local host database
-//mongoose.connect('mongodb://localhost/rotten-potatoes');
-
-//heroku database.
-mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes'), { useNewUrlParser: true });
-
+// local host database
+mongoose.connect('mongodb://localhost/rotten-potatoes');
 
 //views middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
-const Post = require('./models/post');
-const posts = require('./controllers/posts');//(app);
-
-// Set db
-const db = require('./data/reddit-db');
-
-app.get('/', (req, res) => {
-    Post.find({})
-      .then(posts => {
-        res.render("posts-index", { posts });
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-    res.render('posts-index');
-})
-
-app.get('/posts/new', (req, res) => {
-    res.render('posts-new');
-})
-
 app.listen(port);
-
-module.exports = app;
